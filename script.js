a9a94e9374890827c94a091f14e57bd422ba722c
Jibran Jan <jibranjan77@gmail.com>
2024-11-24 16:30:33 +0530

    add event listener to dice roll

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const $score0 = document.querySelector('#score--0');
+const $score1 = document.querySelector('#score--1');
+const $current0 = document.querySelector('#current--0');
+const $current1 = document.querySelector('#current--1');
+
+const $dice = document.querySelector('.dice');
+const $btnNew = document.querySelector('.btn--new');
+const $btnRoll = document.querySelector('.btn--roll');
+const $btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+
+$btnRoll.addEventListener('click', () => {
+    // Generate a random number between 1 and 6
+    const randomNumber = Math.trunc(Math.random() * 6) + 1;
+    
+    // Display number rolled
+    $dice.src = `dice-${randomNumber}.png`;
+    $dice.classList.remove('hidden');
+
+    // Check for rolled number
+    if (randomNumber !== 1) {
+        // Add to current score
+        currentScore += randomNumber;
+        $current0.textContent = currentScore;
+    } else {
+        // Switch to next player
+        
+    }
+});
