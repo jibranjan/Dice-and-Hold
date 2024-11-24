25626594fa4526b3957c9ca4c65cf209ad851ee4
Jibran Jan <jibranjan77@gmail.com>
2024-11-24 18:01:17 +0530

    add logic for  if we rolled 1

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const $score0 = document.querySelector('#score--0');
-const $score1 = document.querySelector('#score--1');
-const $current0 = document.querySelector('#current--0');
-const $current1 = document.querySelector('#current--1');
+const $players = document.querySelectorAll('.player');
+const $score0 = document.getElementById('score--0');
+const $score1 = document.getElementById('score--1');
+const $current0 = document.getElementById('current--0');
+const $current1 = document.getElementById('current--1');
 
 const $dice = document.querySelector('.dice');
 const $btnNew = document.querySelector('.btn--new');
 const $btnRoll = document.querySelector('.btn--roll');
 const $btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
+let activePlayer = 0;
 let currentScore = 0;
 
 $btnRoll.addEventListener('click', () => {
@@ -19,14 +22,21 @@ $btnRoll.addEventListener('click', () => {
     // Display number rolled
     $dice.src = `dice-${randomNumber}.png`;
     $dice.classList.remove('hidden');
+    const $activePlayer = document.getElementById(`current--${activePlayer}`);
 
     // Check for rolled number
     if (randomNumber !== 1) {
         // Add to current score
         currentScore += randomNumber;
-        $current0.textContent = currentScore;
+        $activePlayer.textContent = currentScore;
     } else {
         // Switch to next player
-        
+        currentScore = 0;
+        $activePlayer.textContent = currentScore;
+
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        $players.forEach(player => {
+            player.classList.toggle('player--active');
+        });
     }
 });
